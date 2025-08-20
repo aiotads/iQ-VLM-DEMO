c5e220779c56348a035c68eeae0e3f9a557aa98b
BillyChen39 <billy_chen@innodisk.com>
2025-08-20 14:22:33 +0800

    feat: update Dockerfile

--- docker/env.sh
+++ docker/env.sh
@@ -6,6 +6,6 @@ export GST_PLUGIN_SCANNER=/host_libexec/gstreamer-1.0/gst-plugin-scanner
 
 export XDG_RUNTIME_DIR=/dev/socket/weston
 export WAYLAND_DISPLAY=wayland-1
-export PYTHONPATH=/host_lib/python3.10/site-packages/
+export PYTHONPATH=/host_lib/python3.12/site-packages/:/host_lib/python3.10/site-packages/
 
 "$@"
--- docker/run.sh
+++ docker/run.sh
@@ -10,5 +10,8 @@
 docker run --rm -it \
     --net host \
     --privileged \
+    --shm-size=2g \
     -v /dev/:/dev \
+    -v /usr/lib:/host_lib \
+    -v /usr/libexec:/host_libexec \
     iq-vlm-demo
